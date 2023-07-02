254f563999f328de57a6232bab930e0db1e00e00
mtverlee <matt@doubleangels.com>
2023-07-02 05:58:13 -0600

    Initial public commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,15 @@
+import '/src/js/index.js'
+
 export default function Footer() {
     return (
         <>
-        <footer>
-            <div className="row">
-                <div className="copyright">
-                    <span>© Copyright Matt VerLee 2023.</span>
+            <footer>
+                <div className="row">
+                    <div className="copyright">
+                        <span>© Copyright Matt VerLee 2023.</span>
+                    </div>
                 </div>
-            </div>
-        </footer>
-        <script src="../js/intro.js"></script>
+            </footer>
         </>
     )
 }
