5fe2abcf9299dc29cbc75867ce4ef5d85e3ee555
mtverlee <matt@doubleangels.com>
2023-10-27 20:44:29 -0600

    Fixed bug with FontAwesome

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,41 +1,41 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import AnimationComponent from './Animation';
 
 export default function Intro() {
-  return (
-    <section id="intro">
-      <div id="canvas-wrap">
-        <AnimationComponent />
-        <div className="intro-overlay"></div>
-        <div className="intro-content">
-          <h5>Hello, World.</h5>
-          <h1>I'm Matt.</h1>
-          <p className="intro-position">
-            <span>Developer | Administrator | Student</span>
-          </p>
-          <a className="button stroke" href="#about" title="">
-            Meet Me!
-          </a>
-        </div>
-        <ul className="intro-social">
-          <li>
-            <a href="https://github.com/mtverlee" aria-label="View my Github profile">
-              <FontAwesomeIcon icon={faGithub} />
-            </a>
-          </li>
-          <li>
-            <a href="#contact" aria-label="Contact me by phone">
-              <FontAwesomeIcon icon={faPhone} />
-            </a>
-          </li>
-          <li>
-            <a href="#contact" aria-label="Contact me by email">
-              <FontAwesomeIcon icon={faEnvelope} />
-            </a>
-          </li>
-        </ul>
-      </div>
-    </section>
-  );
+    return (
+        <section id="intro">
+            <div id="canvas-wrap">
+                <AnimationComponent />
+                <div className="intro-overlay"></div>
+                <div className="intro-content">
+                    <h5>Hello, World.</h5>
+                    <h1>I'm Matt.</h1>
+                    <p className="intro-position">
+                        <span>Developer | Administrator | Student</span>
+                    </p>
+                    <a className="button stroke" href="#about" title="">
+                        Meet Me!
+                    </a>
+                </div>
+                <ul className="intro-social">
+                    <li>
+                        <a href="https://github.com/mtverlee" aria-label="View my Github profile">
+                            <FontAwesomeIcon icon={faGithub} />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#contact" aria-label="Contact me by phone">
+                            <FontAwesomeIcon icon={faPhone} />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#contact" aria-label="Contact me by email">
+                            <FontAwesomeIcon icon={faEnvelope} />
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    );
 }
