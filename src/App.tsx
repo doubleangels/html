694586116c2128d38289339714e6870dc4a3a697
Matt VerLee <matt@doubleangels.com>
2023-10-29 17:14:41 -0600

    Move Sentry to earlier in app lifecycle

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,3 @@
-import * as Sentry from "@sentry/react";
-
-Sentry.init({
-  dsn: "https://0a4368d2d941613bda14abf6bca642ae@o244019.ingest.sentry.io/4506085004083200",
-  integrations: [
-    new Sentry.BrowserTracing({
-      tracePropagationTargets: ["localhost", /^https:\/\/doubleangels\.com\//],
-    }),
-    new Sentry.Replay(),
-  ],
-  tracesSampleRate: 1.0,
-  replaysSessionSampleRate: 1.0,
-  replaysOnErrorSampleRate: 1.0,
-});
-
-
 import Intro from './components/Intro';
 import About from './components/About';
 import Resume from './components/Resume';
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://0a4368d2d941613bda14abf6bca642ae@o244019.ingest.sentry.io/4506085004083200",
+  integrations: [
+    new Sentry.BrowserTracing({
+      tracePropagationTargets: ["localhost", /^https:\/\/doubleangels\.com\//],
+    }),
+    new Sentry.Replay(),
+  ],
+  tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 1.0,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
