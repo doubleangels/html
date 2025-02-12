d36b03e896ccb7cf3dab0a0e4d542dcde13d23b0
Matt VerLee <mverlee7@gmail.com>
2025-02-12 03:25:11 -0700

    Remove extra sentry source map upload

--- vite.config.ts
+++ vite.config.ts
@@ -11,10 +11,6 @@ export default defineConfig({
     sentryVitePlugin({
       org: "doubleangels",
       project: "html"
-    }),
-    sentryVitePlugin({
-      org: "doubleangels",
-      project: "html"
     })
   ],
   build: {
