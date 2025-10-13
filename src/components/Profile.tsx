83412ca2b3f9849ee0f4691ce650b1849113253d
doubleangels <matt@doubleangels.com>
2025-10-13 16:05:06 -0600

    Improve visual appearance

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -4,7 +4,7 @@ export default function Profile() {
     return (
         <div className="col-twelve tab-full">
             <h3 className="profile-title">{data.profileData.profileTitle}</h3>
-            <p className="profile-description">
+            <p className="lead profile-description">
                 {data.profileData.profileDescription}
             </p>
         </div>
